1a03ef0237960fc3348abf419106229c71646c0b
Khanh Dinh <dqkcode@gmail.com>
2020-01-10 17:03:41 +0700

    structure config env

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dqkcode/movie-database
+
+go 1.13
---
+++ internal/pkg/config/envconfig/env.go
@@ -0,0 +1,14 @@
+package envconfig
+
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+var envPrefix=""
+
+func Load(t interface{})  {
+	if err := envconfig
+	
+	
+}
